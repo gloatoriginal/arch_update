6a9a1c121949a9a1ce951665796b44e603966a59
Devin Baughman <47373236+gloatoriginal@users.noreply.github.com>
2024-11-18 14:49:42 -0800

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Attempt at a oneshot installation and setup of update script from git.com/gloatoriginal/arch_update"
+echo "Lets get sudo out of the way with a quick download of cronie, kexec and flatpak"
+sudo pacman -S cronie kexec-tools flatpak \
+        && sudo systemctl enable --now cronie
+
+git clone https://github.com/gloatoriginal/arch_update \ 
+        && chmod +x ./arch_update/update.sh \
+        && sudo cp ./arch_update/unmodeset.service /etc/systemd/system/ \
+        && sudo systemctl enable unmodeset.service \
+        && sudo cp ./arch_update/kexec-load@.service /etc/systemd/system/ 
+
+echo "cronjobs have not been set and require YOU the user to set them up, but cronie is enable in your systemctl."
+ 
