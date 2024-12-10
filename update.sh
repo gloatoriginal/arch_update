49b7fe4ae6167c280b586578bc5c5443c177075b
Devin Baughman <47373236+gloatoriginal@users.noreply.github.com>
2024-12-09 22:40:03 -0800

    Update update.sh
    hopefully this does it

--- update.sh
+++ update.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
-
 date
-
 # Update and upgrade packages with single command sequence
-pacman -Syu --noconfirm \
+output=$(pacman -Syu --noconfirm \
         && paccache -r -k 1 \
-        && flatpak upgrade -y \
-        &> temp_out.txt
+        && flatpak upgrade -y)
 
+echo "$output"
 
-output=$(cat temp_out.txt)
-echo $output
-rm temp_out.txt
-# Grab "linux" from the OS
-k_name=$(uname -s | tr '[:upper:]' '[:lower:]')
-
-## Extract the -lts or -rt if that's the current kernel
-k_ver=$(uname -r | rev | cut -d '-' -f 1 | rev)
+k_name=$(uname -s \
+        | tr '[:upper:]' '[:lower:]')
+        
+k_ver=$(uname -r \
+        | rev \
+        | cut -d '-' -f 1 \
+        | rev)
+        
 if ! [[ $k_ver =~ ^[0-9]+$ ]]; then k_name="$k_name-$k_ver"; fi
 
-
 if [[ $output =~ $k_name ]]; then
-        echo "Reboot" && date && sleep 5
-        shutdown -r now
+        echo "Reboot" \
+                && date \
+                && sleep 5 \
+                && shutdown -r now
 else
         echo "kexec into: $k_name" \
                 && date \
-                && sleep 5
-        systemctl start kexec-load@$k_name.service \
+                && sleep 5 \ 
+                && systemctl start kexec-load@$k_name.service \
                 && systemctl start kexec.target
 fi
